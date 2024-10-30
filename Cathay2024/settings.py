238294ed534469d5fd6aa05f12d6a423257abb6a
Jasminder <sjasminder94@gmail.com>
2024-10-30 22:33:51 +0800

    changed to mysql

--- Cathay2024/settings.py
+++ Cathay2024/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'Cathay2024.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'test',
+        'USER' : 'root',
+        'PASSWORD' : 'Jazzy1300',
+        'HOST': 'localhost',
+        'PORT': '3300',
     }
 }
 
