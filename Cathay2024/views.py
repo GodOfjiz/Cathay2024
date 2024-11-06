61d5ee3f145b6d10111958b806975152d1378f76
Jasminder <sjasminder94@gmail.com>
2024-11-07 05:42:40 +0800

    activity_function

--- Cathay2024/views.py
+++ Cathay2024/views.py
@@ -1,6 +1,7 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from . import Activity, Route, Tickets
+import json
 
 @api_view(['GET', 'POST'])
 def hello_world(request):
@@ -10,11 +11,16 @@ def hello_world(request):
 
 @api_view(['GET', 'POST'])
 def activity_seeking(request):
-    answer = Activity.GETactivityloc(destination = 'Osaka', no_of_suggestions = 10)
+    if request.method == 'GET':
+        data = json.loads(request.body)
+        location = data.get('location', 'Osaka')  # Default to 'Osaka' if not provided
+        no_of_suggestions = data.get('no_of_suggestions', 10)  # Default to 10 if not provided
+        answer = Activity.GETactivityloc(destination = location, no_of_suggestions = no_of_suggestions)
     return Response(answer)
 
 @api_view(['GET', 'POST'])
 def route_optimize(request):
+    
     answer = Route.POSTroute(destination = 'Rovaniemi', origin = 'GÃ¶teborg Landvetter Airport', travel_mode = ["TRAIN"], route_pref = 'LESS_WALKING', alternative = False)
     return Response(answer)
 
